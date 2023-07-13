197ce914b50eb2796e0cbd1840703b2992ece394
mrkataei <kouroshinfo96@gmail.com>
2023-07-13 20:35:53 +0330

    d

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,57 @@
+import React from "react";
+
+function CompA() {
+  return (
+    <div class="float-container">
+      <div class="float-child">
+        <div class="green">Float Column 1</div>
+      </div>
+      <div class="float-child">
+        <div class="blue">Float Column 2</div>
+      </div>
+    </div>
+  );
+}
+
+function CompB() {
+  return <h1>CompB</h1>;
+}
+
+class CompC extends React.Component {
+  render() {
+    return <h1>CompC</h1>;
+  }
+}
+
+const CompD = () => {
+  return <div>I am arrow function</div>;
+};
+
+const CompH = (_) => {
+  return <div>I am arrow function</div>;
+};
+
+const CompG = (params) => {
+  return <div>I am arrow function</div>;
+};
+
+function CompI() {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("h1", null, "JSX"),
+    React.createElement(CompD)
+  );
+}
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <CompA />
+      <CompB />
+      <CompC />
+      <CompD />
+      <CompI />
+    </div>
+  );
 }
