c186adb62eda71eb3b0840fa10cbea59424bb6ab
mrkataei <kouroshinfo96@gmail.com>
2023-07-20 22:41:06 +0330

    bulma

---
+++ src/pages/index-cop.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+
+function CompA() {
+  return (
+    <div class="float-container">
+      <div class="float-child">
+        <div class="green">Float Column 1</div>
+      </div>
+      <div class="float-child">
+        <div class="blue">Float Column 2</div>
+      </div>
+    </div>
+  );
+}
+
+function CompB(props) {
+  return (
+    <>
+      <div>my value: {props.Myprop1}</div>
+      <div>my value: {<props.promp2 />}</div>
+    </>
+  );
+}
+
+class CompC extends React.Component {
+  // state = {
+  //   myValue: 10,
+  // };
+
+  constructor() {
+    super();
+    this.state = {
+      myValue: 10,
+    };
+  }
+
+  changeState(incrementor) {
+    // console.log(incrementor);
+    this.setState({
+      myValue: incrementor,
+    });
+  }
+  render() {
+    const myValue = this.state.myValue;
+    return (
+      <>
+        <h1>CompC</h1>
+        current state : <h1>{myValue}</h1>
+        <button onClick={() => this.changeState(myValue + 1)}>+</button>
+        <button onClick={() => this.changeState(myValue - 1)}>-</button>
+      </>
+    );
+  }
+}
+
+const CompD = () => {
+  return <div>I am arrow function</div>;
+};
+
+const CompH = (_) => {
+  return <div>I am arrow function</div>;
+};
+
+const CompG = (params) => {
+  return <div>I am arrow function</div>;
+};
+
+function CompI(props) {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("h1", null, "JSX"),
+    React.createElement(CompD)
+  );
+}
+export default function Home() {
+  const valueState = useState(10);
+  const [value, setValue] = useState(20);
+  console.log(valueState);
+  console.log(valueState[0]);
+  useEffect(
+    ()=> {
+      console.log("hi")
+    }, [value]
+  )
+  const increment = () => {
+    setValue(value + 1);
+  };
+  const decrement = () => {
+    setValue(value - 1);
+  };
+  return (
+    <div>
+      valueState: {valueState[0]}
+      <br></br>
+      valueState: <h1>{value}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={() => setValue(value - 1)}>-</button>
+      <h1>Hello world!</h1>
+      <CompA />
+      <CompC />
+      <CompD />
+      <CompB Myprop1={value} promp2={() => <div>my new comp</div>} />
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,102 +1,212 @@
-import React, { useState } from "react";
-
-function CompA() {
-  return (
-    <div class="float-container">
-      <div class="float-child">
-        <div class="green">Float Column 1</div>
-      </div>
-      <div class="float-child">
-        <div class="blue">Float Column 2</div>
-      </div>
-    </div>
-  );
-}
-
-function CompB(props) {
+export default function Home() {
   return (
     <>
-      <div>my value: {props.Myprop1}</div>
-      <div>my value: {<props.promp2 />}</div>
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="../">
+              <img src="../images/bulma.png" alt="Logo" />
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenu">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id="navbarMenu" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item is-active">Home</a>
+              <a class="navbar-item">Examples</a>
+              <a class="navbar-item">Features</a>
+              <a class="navbar-item">Team</a>
+              <a class="navbar-item">Archives</a>
+              <a class="navbar-item">Help</a>
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">Account</a>
+                <div class="navbar-dropdown">
+                  <a class="navbar-item">Dashboard</a>
+                  <a class="navbar-item">Profile</a>
+                  <a class="navbar-item">Settings</a>
+                  <hr class="navbar-divider" />
+                  <div class="navbar-item">Logout</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <section class="hero is-info is-medium is-bold">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />
+              sed eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </h1>
+          </div>
+        </div>
+      </section>
+      <div class="container">
+        <section class="articles">
+          <div class="column is-8 is-offset-2">
+            <div class="card article">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-content has-text-centered">
+                    <p class="title article-title">
+                      Introducing a new feature for paid subscribers
+                    </p>
+                    <div class="tags has-addons level-item">
+                      <span class="tag is-rounded is-info">@skeetskeet</span>
+                      <span class="tag is-rounded">May 10, 202X</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="content article-body">
+                  <p>
+                    Non arcu risus quis varius quam quisque. Dictum varius duis
+                    at consectetur lorem. Posuere sollicitudin aliquam ultrices
+                    sagittis orci a scelerisque purus semper.{" "}
+                  </p>
+                  <p>
+                    Metus aliquam eleifend mi in nulla posuere sollicitudin
+                    aliquam ultrices. In hac habitasse platea dictumst
+                    vestibulum rhoncus est pellentesque elit. Accumsan lacus vel
+                    facilisis volutpat. Non sodales neque sodales ut etiam. Est
+                    pellentesque elit ullamcorper dignissim cras tincidunt
+                    lobortis feugiat vivamus.
+                  </p>
+                  <h3 class="has-text-centered">
+                    How to properly center tags in bulma?
+                  </h3>
+                  <p>
+                    {" "}
+                    Proper centering of tags in bulma is done with class:{" "}
+                    <pre>level-item</pre>
+                    Voluptat ut farmacium tellus in metus vulputate. Feugiat in
+                    fermentum posuere urna nec. Pharetra convallis posuere morbi
+                    leo urna molestie. Accumsan lacus vel facilisis volutpat est
+                    velit egestas. Fermentum leo vel orci porta. Faucibus
+                    interdum posuere lorem ipsum.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card article">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-center">
+                    <img
+                      src="https://res.cloudinary.com/ameo/image/upload/v1639144778/typocat_svbspx.png"
+                      class="author-image"
+                      alt="Placeholder image"
+                    />
+                  </div>
+                  <div class="media-content has-text-centered">
+                    <p class="title article-title">
+                      Sapien eget mi proin sed 🔱
+                    </p>
+                    <p class="subtitle is-6 article-subtitle">
+                      <a href="#">@daria</a> on February 17, 202X
+                    </p>
+                  </div>
+                </div>
+                <div class="content article-body">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Accumsan lacus vel facilisis volutpat est velit
+                    egestas. Sapien eget mi proin sed. Sit amet mattis vulputate
+                    enim.
+                  </p>
+                  <p>
+                    Commodo ullamcorper a lacus vestibulum sed arcu. Fermentum
+                    leo vel orci porta non. Proin fermentum leo vel orci porta
+                    non pulvinar. Imperdiet proin fermentum leo vel. Tortor
+                    posuere ac ut consequat semper viverra. Vestibulum lectus
+                    mauris ultrices eros.
+                  </p>
+                  <h3 class="has-text-centered">
+                    Lectus vestibulum mattis ullamcorper velit sed ullamcorper
+                    morbi. Cras tincidunt lobortis feugiat vivamus.
+                  </h3>
+                  <p>
+                    In eu mi bibendum neque egestas congue quisque egestas diam.
+                    Enim nec dui nunc mattis enim ut tellus. Ut morbi tincidunt
+                    augue interdum velit euismod in. At in tellus integer
+                    feugiat scelerisque varius morbi enim nunc. Vitae suscipit
+                    tellus mauris a diam. Arcu non sodales neque sodales ut
+                    etiam sit amet.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <section class="hero is-info is-bold is-small promo-block">
+              <div class="hero-body">
+                <div class="container">
+                  <h1 class="title">
+                    <i class="fa fa-bell-o"></i> Nemo enim ipsam voluptatem
+                    quia.
+                  </h1>
+                  <span class="tag is-black is-medium is-rounded">
+                    Natus error sit voluptatem
+                  </span>
+                </div>
+              </div>
+            </section>
+            <div class="card article">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-center">
+                    <img
+                      src="https://res.cloudinary.com/ameo/image/upload/v1639144778/typocat_svbspx.png"
+                      class="author-image"
+                      alt="Placeholder image"
+                    />
+                  </div>
+                  <div class="media-content has-text-centered">
+                    <p class="title article-title">
+                      Cras tincidunt lobortis feugiat vivamus.
+                    </p>
+                    <p class="subtitle is-6 article-subtitle">
+                      <a href="#">@angela</a> on June 7, 202X
+                    </p>
+                  </div>
+                </div>
+                <div class="content article-body">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Accumsan lacus vel facilisis volutpat est velit
+                    egestas. Sapien eget mi proin sed. Sit amet mattis vulputate
+                    enim.
+                  </p>
+                  <p>
+                    Commodo ullamcorper a lacus vestibulum sed arcu. Fermentum
+                    leo vel orci porta non. Proin fermentum leo vel orci porta
+                    non pulvinar. Imperdiet proin fermentum leo vel. Tortor
+                    posuere ac ut consequat semper viverra. Vestibulum lectus
+                    mauris ultrices eros.
+                  </p>
+                  <h3 class="has-text-centered">
+                    “Everyone should be able to do one card trick, tell two
+                    jokes, and recite three poems, in case they are ever trapped
+                    in an elevator.”
+                  </h3>
+                  <p>
+                    In eu mi bibendum neque egestas congue quisque egestas diam.
+                    Enim nec dui nunc mattis enim ut tellus. Ut morbi tincidunt
+                    augue interdum velit euismod in. At in tellus integer
+                    feugiat scelerisque varius morbi enim nunc. Vitae suscipit
+                    tellus mauris a diam. Arcu non sodales neque sodales ut
+                    etiam sit amet.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <script async type="text/javascript" src="../js/bulma.js"></script>
     </>
-  );
-}
-
-class CompC extends React.Component {
-  // state = {
-  //   myValue: 10,
-  // };
-
-  constructor() {
-    super();
-    this.state = {
-      myValue: 10,
-    };
-  }
-
-  changeState(incrementor) {
-    // console.log(incrementor);
-    this.setState({
-      myValue: incrementor,
-    });
-  }
-  render() {
-    const myValue = this.state.myValue;
-    return (
-      <>
-        <h1>CompC</h1>
-        current state : <h1>{myValue}</h1>
-        <button onClick={() => this.changeState(myValue + 1)}>+</button>
-        <button onClick={() => this.changeState(myValue - 1)}>-</button>
-      </>
-    );
-  }
-}
-
-const CompD = () => {
-  return <div>I am arrow function</div>;
-};
-
-const CompH = (_) => {
-  return <div>I am arrow function</div>;
-};
-
-const CompG = (params) => {
-  return <div>I am arrow function</div>;
-};
-
-function CompI(props) {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "JSX"),
-    React.createElement(CompD)
-  );
-}
-export default function Home() {
-  const valueState = useState(10);
-  const [value, setValue] = useState(20);
-  console.log(valueState);
-  console.log(valueState[0]);
-
-  const increment = () => {
-    setValue(value + 1);
-  };
-  const decrement = () => {
-    setValue(value - 1);
-  };
-  return (
-    <div>
-      valueState: {valueState[0]}
-      <br></br>
-      valueState: <h1>{value}</h1>
-      <button onClick={increment}>+</button>
-      <button onClick={() => setValue(value - 1)}>-</button>
-      <h1>Hello world!</h1>
-      <CompA />
-      <CompC />
-      <CompD />
-      <CompB Myprop1={value} promp2={() => <div>my new comp</div>} />
-    </div>
   );
 }
