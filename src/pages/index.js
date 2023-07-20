ee5e6ff77011b394db8180da0476a8e1eaaafb68
mrkataei <kouroshinfo96@gmail.com>
2023-07-21 01:03:15 +0330

    resources

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@ import Layout from "components/Layout";
 import ResourceHighlight from "components/ResourceHighlight";
 import NewsLetter from "components/NewsLetter";
 
+import { resources } from "api/data";
+
 export default function Home() {
   return (
     <>
       <Layout>
         <ResourceHighlight />
         <NewsLetter />
+        {JSON.stringify(resources)}
       </Layout>
 
       <script async type="text/javascript" src="../js/bulma.js"></script>
