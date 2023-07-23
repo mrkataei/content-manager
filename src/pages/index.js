c399844bae616d635519ce6e00efd84b82b96d78
mrkataei <kouroshinfo96@gmail.com>
2023-07-23 18:50:29 +0330

    Get static props api call

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import Layout from "components/Layout";
 import ResourceHighlight from "components/ResourceHighlight";
 import NewsLetter from "components/NewsLetter";
 
-import { resources } from "api/data";
+// import { resources } from "api/data";
 
-export default function Home() {
+export default function Home({ resources }) {
   return (
     <>
       <Layout>
@@ -16,4 +16,23 @@ export default function Home() {
       <script async type="text/javascript" src="../js/bulma.js"></script>
     </>
   );
+}
+
+// export function getStaticProps(){
+//   return{
+//     props:{
+//       myData1: [1,2,3],
+//       myData2: ["a", "b", "c"]
+//     }
+//   }
+// }
+
+export async function getStaticProps() {
+  const resData = await fetch("http://localhost:3000/api/resources");
+  const data = await resData.json();
+  return {
+    props: {
+      resources: data,
+    },
+  };
 }
